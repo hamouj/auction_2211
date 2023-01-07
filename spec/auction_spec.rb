7e52df0f2a0e08134f15871d55c41ba4900e80a1
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-07 10:14:29 -0800

    Test: create #bidders test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -111,4 +111,21 @@ describe Auction do
       expect(auction.potential_revenue).to eq(87)
     end
   end
+
+  describe '#bidders' do
+    it 'returns an array of attendees who have placed a bid in the auction' do
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+
+      item1.add_bid(attendee2, 20)
+      item1.add_bid(attendee1, 22)
+      item4.add_bid(attendee3, 50)
+      item3.add_bid(attendee2, 15)
+
+      expect(auction.bidders).to eq([attendee2, attendee1, attendee3])
+    end
+  end
 end
