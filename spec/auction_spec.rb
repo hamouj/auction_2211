95a3a914792b9aa83e21145a3bfe5ebc2aab037d
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-07 10:00:01 -0800

    Test: create #unpopular_items tests

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -77,4 +77,21 @@ describe Auction do
       expect(item1.current_high_bid).to eq(22)
     end
   end
+
+  describe '#unpopular_items' do
+    it 'lists items with no bids' do
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+
+      item1.add_bid(attendee2, 20)
+      item1.add_bid(attendee1, 22)
+      item4.add_bid(attendee3, 50)
+
+      expect(auction.unpopular_items).to eq([item2, item3, item5])
+      expect(auction.unpopular_items.size).to eq(3)
+    end
+  end
 end
