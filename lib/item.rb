ed1494032020ad101b78f4333b84907d747fb790
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-07 09:57:44 -0800

    Feat: create #current_high_bid

--- lib/item.rb
+++ lib/item.rb
@@ -9,4 +9,12 @@ class Item
   def add_bid(attendee, bid_amount)
     @bids[attendee] = bid_amount
   end
+
+  def current_high_bid
+    highest_bid_set = 
+    @bids.max_by do |attendee, bid_amount|
+      bid_amount
+    end
+    highest_bid = highest_bid_set[1]
+  end
 end
