13e3a72f36a81889a4f2437ee73a41e81f649c6f
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2023-01-07 09:48:22 -0800

    Test: create #add_bid test

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -5,7 +5,14 @@ require './lib/auction'
 describe Auction do
   let(:item1) {Item.new('Chalkware Piggy Bank')}
   let(:item2) {Item.new('Bamboo Picture Frame')}
-  let(:attendee) {Attendee.new(name: 'Megan', budget: '$50')}
+  let(:item3) {Item.new('Homemade Chocolate Chip Cookies')}
+  let(:item4) {Item.new('2 Days Dogsitting')}
+  let(:item5) {Item.new('Forever Stamps')}
+
+  let(:attendee1) {Attendee.new(name: 'Megan', budget: '$50')}
+  let(:attendee2) {Attendee.new(name: 'Bob', budget: '$75')}
+  let(:attendee3) {Attendee.new(name: 'Mike', budget: '$100')}
+
   let(:auction) {Auction.new}
 
   describe '#initialize' do
@@ -33,6 +40,26 @@ describe Auction do
       auction.add_item(item2)
 
       expect(auction.item_names).to eq(["Chalkware Piggy Bank", "Bamboo Picture Frame"])
+    end
+  end
+
+  describe '#add_bid' do
+    it 'adds bids to items' do
+      auction.add_item(item1)
+      auction.add_item(item2)
+      auction.add_item(item3)
+      auction.add_item(item4)
+      auction.add_item(item5)
+
+      item1.add_bid(attendee2, 20)
+      item1.add_bid(attendee1, 22)
+
+      expected_hash = {
+        attendee2 => 20
+        attendee1 => 22
+      }
+
+      expect(item1.bids).to eq(expected_hash)
     end
   end
 end
